ff485aac624683648087dfeb5dbf88f3314e860e
ljj <3310093920@qq.com>
2020-06-24 13:46:05 +0800

    实现按键

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.gitdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addBtn;
+    TextView tx;
+    private int num=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addBtn.findViewById(R.id.add);
+        tx.findViewById(R.id.num);
+        tx.setText(num);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num++;
+            }
+        });
+
     }
 }
